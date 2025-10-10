4324772c6cd6d66363845d62583af70080113d61
GGalant <ggalant@frba.utn.edu.ar>
2025-10-10 10:23:56 -0300

    Agregue funciones
    Co-authored-by: CamilaDiPlacido <CamilaDiPlacido@users.noreply.github.com>
    Co-authored-by: Javier Maita <javiermaita@hotmail.com.ar>
    Co-authored-by: Sofia Costante <scostantesepulveda@users.noreply.github.com>

--- compilador_micro/chatResolucion.c
+++ compilador_micro/chatResolucion.c
@@ -72,6 +72,41 @@ typedef struct
 char buffer[TAMLEX];
 TOKEN tokenActual;
 int flagToken = 0;
+/*************************Prototipos de Funciones*********************/
+TOKEN scanner();
+int columna(int c);
+int estadoFinal(int e);
+void Objetivo(void);
+void Programa(void);
+void ListaSentencias(void);
+void Sentencia(void);
+void ListaIdentificadores(void);
+void Identificador(REG_EXPRESION * presul);
+void ListaExpresiones(void);
+void Expresion(REG_EXPRESION * presul);
+void Primaria(REG_EXPRESION * presul);
+void OperadorAditivo(char * presul);
+
+REG_EXPRESION ProcesarCte(void);
+REG_EXPRESION ProcesarId(void);
+char * ProcesarOp(void);
+void Leer(REG_EXPRESION in);
+void Escribir(REG_EXPRESION out);
+REG_EXPRESION GenInfijo(REG_EXPRESION e1, char * op, REG_EXPRESION e2);
+
+void Match(TOKEN t);
+TOKEN ProximoToken();
+void ErrorLexico();
+void ErrorSintactico();
+void Generar(char * co, char * a, char * b, char * c);
+char * Extraer(REG_EXPRESION * preg);
+int Buscar(char * id, RegTS * TS, TOKEN * t);
+void Colocar(char * id, RegTS * TS);
+void Chequear(char * s);
+void Comenzar(void);
+void Terminar(void);
+void Asignar(REG_EXPRESION izq, REG_EXPRESION der);
+
 
 /* === AGREGADOS: prototipos para PAS nuevas y helpers de etiquetas/condición === */
 void SentenciaSi(void);
@@ -79,6 +114,7 @@ void SentenciaMientras(void);
 void SentenciaRepetir(void);
 void Condicion(REG_EXPRESION *presul);
 void NuevaEtiqueta(char *out);
+
 
 /**************************Scanner************************************/
 
@@ -435,7 +471,6 @@ void SentenciaRepetir(void)
     /* <sentencia> -> REPETIR <listaSentencias> HASTA <condicion> ; */
     REG_EXPRESION c;
     char Linicio[TAMLEX];
-
     Match(REPETIR);
     NuevaEtiqueta(Linicio);
     Generar("Etiqueta", Linicio, "", "");
